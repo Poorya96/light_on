e90856c93dba53803384298a56cfc51640acafaa
P0orya <poorya_1@outlook.com>
2023-07-17 21:36:18 +0330

    add light on function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
-  })
-);
+const btn = document.getElementById('btn');
+
+function toggleBtn() {
+  btn.classList.toggle("active");
+  light.classList.toggle("light_on");
+
+}
+
+// const collapsibles = document.querySelectorAll(".collapsible");
+// collapsibles.forEach((item) =>
+//   item.addEventListener("click", function () {
+//     this.classList.toggle("collapsible--expanded");
+//   })
+// );
